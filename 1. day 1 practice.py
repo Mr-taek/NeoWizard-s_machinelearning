38c0d71636fca0ce6bf86be40deb951f8064ea09
Mr.tek <dlrmsxor9856@gmail.com>
2021-01-21 11:06:23 +0900

    "

---
+++ 1. day 1 practice.py
@@ -0,0 +1,17 @@
+a='hello'
+b={'kim':90, 'lee':[10,20,30]}
+
+c=list(a)
+d=list(b)
+e=list(b.values())
+print(c,d,type(c),type(d))
+print(b.values(),type(b.values()))
+print(e,type(e))
+
+a='1231'
+a1=1231.12
+b=int(a)
+b1=int(a1)
+c=list(a)
+print(b1)
+print(b,c)
