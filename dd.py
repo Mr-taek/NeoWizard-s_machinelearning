fb4c6208cbdae63c9137a06a7a73d6ffd051aa7c
Mr.tek <dlrmsxor9856@gmail.com>
2021-01-26 10:14:35 +0900

    ㄴ

--- dd.py
+++ dd.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-a=np.array([[1,2,3],[3,4,5]])
+q=np.array([[[4,2,1,2],[1,2,3,4],[3,4,5,6]],[[3,4,1,2],[10,11,2,40],[30,40,11,21]]])
 
-row=np.array([7,8,9]) # 그냥 이대로만 두면 (3,1)의 벡터가 되므로 행열로 만들어 줘야 한다.
-row=row.reshape(1,3)
-print(row)
-
-colum=np.array([1000,2000]).reshape(2,1)
-
-b=np.concatenate((a,row),axis=0)
-d=np.concatenate((a,colum),axis=0)
-
-print(b)
-print(d)
+x=np.max(q,axis=1)
+y=np.max(q,axis=0)
+x1=np.argmax(q,axis=1)
+y1=np.argmax(q,axis=0)
+print(q)
+#print(x)
+#print(x1)
+print(y)
+print(y1)
